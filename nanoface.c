ce86d0383bd8645982836a89cecbba0cf829c538
Lauri Niskanen <ape@ape3000.com>
2014-11-09 15:08:42 +0200

    Fix CHECKPATCH warnings

--- nanoface.c
+++ nanoface.c
@@ -20,7 +20,7 @@ static unsigned char init_data[] = {};
 
 static void init_complete_callback(struct urb *request)
 {
-	printk(KERN_INFO "ALVA Nanoface initialized\n");
+	dev_info(&request->dev->dev, "ALVA Nanoface initialized\n");
 	usb_free_urb(request);
 }
 
@@ -31,16 +31,17 @@ static int nanoface_probe(struct usb_interface *interface,
 	struct usb_device *dev;
 	struct urb *init_request;
 
+	dev = interface_to_usbdev(interface);
+
 	init_request = usb_alloc_urb(0, 0);
 	if (init_request == 0) {
-		printk(KERN_ERR "ALVA Nanoface initialization failed: Cannot allocate memory for URB request\n");
+		dev_err(&dev->dev, "ALVA Nanoface initialization failed: Cannot allocate memory for URB request\n");
 		return -ENOMEM;
 	}
 
-	printk(KERN_INFO "ALVA Nanoface (%04X:%04X) connected\n",
-	       id->idVendor, id->idProduct);
+	dev_info(&dev->dev, "ALVA Nanoface (%04X:%04X) connected\n",
+		 id->idVendor, id->idProduct);
 
-	dev = interface_to_usbdev(interface);
 	usb_fill_control_urb(init_request, dev,
 			     usb_sndctrlpipe(dev, 0), init_setup,
 			     init_data, sizeof(init_data),
@@ -48,7 +49,7 @@ static int nanoface_probe(struct usb_interface *interface,
 
 	status = usb_submit_urb(init_request, 0);
 	if (status != 0) {
-		printk(KERN_ERR "ALVA Nanoface initialization failed: Error %d when submitting URB\n",
+		dev_err(&dev->dev, "ALVA Nanoface initialization failed: Error %d when submitting URB\n",
 		       status);
 		return status;
 	}
@@ -59,35 +60,33 @@ static int nanoface_probe(struct usb_interface *interface,
 
 static void nanoface_disconnect(struct usb_interface *interface)
 {
-	printk(KERN_INFO "ALVA Nanoface disconnected\n");
+	dev_info(&interface->dev, "ALVA Nanoface disconnected\n");
 }
- 
-static struct usb_device_id nanoface_table[] =
-{
+
+static struct usb_device_id nanoface_table[] = {
 	{ USB_DEVICE(0x0a4a, 0xaffe) },
 	{ /* Terminating entry */ }
 };
 
-MODULE_DEVICE_TABLE (usb, nanoface_table);
- 
-static struct usb_driver nanoface_driver =
-{
+MODULE_DEVICE_TABLE(usb, nanoface_table);
+
+static struct usb_driver nanoface_driver = {
 	.name = "nanoface",
 	.id_table = nanoface_table,
 	.probe = nanoface_probe,
 	.disconnect = nanoface_disconnect,
 };
- 
+
 static int __init nanoface_init(void)
 {
 	return usb_register(&nanoface_driver);
 }
- 
+
 static void __exit nanoface_exit(void)
 {
 	usb_deregister(&nanoface_driver);
 }
- 
+
 module_init(nanoface_init);
 module_exit(nanoface_exit);
 
